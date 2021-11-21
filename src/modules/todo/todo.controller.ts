53cc048e43108b2aa918cb569c168f15fedc7f9c
Aleksandr Kazanin <kazanin@arview.pro>
2021-11-21 16:09:33 +0700

    change todo controller to work with rabbit mq

--- src/modules/todo/todo.controller.ts
+++ src/modules/todo/todo.controller.ts
@@ -1,34 +1,34 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import { TodoService } from './todo.service';
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('todo')
 export class TodoController {
-  constructor(private readonly todoService: TodoService) {}
+  constructor(@Inject('TODOS_SERVICE') private readonly client: ClientProxy) {}
 
   @Post()
   create(@Body() createTodoDto: CreateTodoDto) {
-    return this.todoService.create(createTodoDto);
+    return this.client.send('create-todo', createTodoDto);
   }
 
   @Get()
   findAll() {
-    return this.todoService.findAll();
+    return this.client.send('get-todos', '');
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.todoService.findOne(id);
+    return this.client.send('get-todo', id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
-    return this.todoService.update(id, updateTodoDto);
+    return this.client.send('update-todo', { id, updateTodoDto });
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.todoService.remove(id);
+    return this.client.send('remove-todo', id);
   }
 }
--- src/modules/todo/todo.module.ts
+++ src/modules/todo/todo.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'TODOS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://guest:guest@localhost:5672'],
+          queue: 'todos-queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [TodoController],
   providers: [TodoService],
 })
